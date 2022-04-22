355f9399a4dd9f3f89549fa73db91c21baffeb7c
Amarjeet <amarjeetd956@gmail.com>
2022-04-23 01:09:11 +0530

    working with javascript

---
+++ shared.js
@@ -0,0 +1,33 @@
+var backdrop = document.querySelector('.backdrop');
+
+var modal =  document.querySelector('.modal');
+var modalNoButton = document.querySelector('.modal__action--negative');
+var selectPlanButtons = document.querySelectorAll('.plan button');
+
+// console.dir(backdrop);
+// console.dir(selectPlanButtons);
+
+for(var i = 0; i<selectPlanButtons.length; i++){
+    selectPlanButtons[i].addEventListener('click' , function() {
+        modal.style.display = 'block';
+        backdrop.style.display = 'block';
+
+        
+    });
+}
+
+
+backdrop.addEventListener('click', function() {
+    backdrop.style.display = 'none';
+    modal.style.display = 'none';
+
+
+});
+
+
+modalNoButton.addEventListener('click', function() {
+    backdrop.style.display = 'none';
+    modal.style.display = 'none';
+
+
+});
